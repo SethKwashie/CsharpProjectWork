ebca9938a57325f6c88e16b441cf5a2392a729de
SethKwashie <seth.kwashie@turntabl.io>
2020-10-16 10:05:32 +0000

    Added nobodyhome exception

---
+++ NobodyHomeException.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Security.Cryptography.X509Certificates;
+
+public class NobodyHomeException : Exception
+{
+	public NobodyHomeException()
+	{
+		public NobodyHomeException(string message) {
+			base(message);
+		}
+	}
+}
