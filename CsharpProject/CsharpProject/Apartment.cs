93565a8b1ccbd99e10ac5afcfff316696eccb80e
Willington9 <willington.bedu-addo@turntabl.io>
2020-10-16 11:59:56 +0000

    "Apartment Class"

---
+++ CsharpProject/CsharpProject/Apartment.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CsharpProject
+{
+    abstract class Apartment
+    {
+        string FloorNumber;
+        string DoorNumber;
+        string ResidentName;
+
+        public abstract string Ring();
+    }
+}
