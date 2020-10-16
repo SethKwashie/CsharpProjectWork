6685a8b018d5e1a998799ca2327f8ae2856c1f8c
Willington9 <willington.bedu-addo@turntabl.io>
2020-10-16 11:17:37 +0000

    "Apartment Class"

--- CsharpProject/CsharpProject/Program.cs
+++ CsharpProject/CsharpProject/Program.cs
@@ -2,8 +2,18 @@
 
 namespace CsharpProject
 {
+    abstract class Apartment
+    {
+        string FloorNumber;
+        string DoorNumber;
+        string ResidentName;
+
+        public abstract string Ring();
+
+    }
     class Program
     {
+        
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
